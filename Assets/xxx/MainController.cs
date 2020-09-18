156ff93209f9f8fc6e86f56cff33ca4f2540ebb5
Luis V. Penados D <lpenados@gmail.com>
2020-09-18 17:49:22 -0600

    commit inicial, medio funcionando

---
+++ Assets/xxx/MainController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+
+    public string _World;
+    public GameObject _SpawnerPrefab;
+
+    // Map<int,Map<int,GameObject>> _WorldCells;
+    Dictionary<int, Dictionary<int, GameObject>> _WorldCells;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_SpawnerPrefab.transform.
+        _World = @"
+        1,2,1,1,2,2,1,2,1,1
+        0,1,0,0,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,0
+        1,2,1,1,2,2,1,2,1,1,1,2,1,1,2,2,1,2,1,1
+        0,1,0,0,1,1,0,1,0,0
+        1,2,1,1,2,2,1,2,1,1,1,2,1,1,2,2,1,2,1,1
+        0,1,0,0,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,0
+        1,2,1,1,2,2,1,2,1,1
+        0,1,0,0,1,1,0,1,0,0,1,2,1,1,2,2,1,2,1,1
+        1,2,1,1,2,2,1,2,1,1,0,1,0,0,1,1,0,1,0,0
+        0,1,0,0,1,1,0,1,0,0
+        1,2,1,1,2,2,1,2,1,1,0,1,0,0,1,1,0,1,0,0
+        0,1,0,0,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,0
+        1,2,1,1,2,2,1,2,1,1
+        0,1,0,0,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,0
+        1,2,1,1,2,2,1,2,1,1,1,2,1,1,2,2,1,2,1,1,1,2,1,1,2,2,1,2,1,1
+        0,1,0,0,1,1,0,1,0,0
+        ";
+        _SpawnAll();
+    }
+
+    void _SpawnAll()
+    {
+
+        var trows = _World.Trim().Split('\n');
+        var jj = 0;
+        // var ttrow= new Dictionary<int,GameObject>();
+        foreach (var trow in trows)
+        {
+            jj++;
+            var ii = 0;
+            var tcolumns = trow.Trim().Split(',');
+            foreach (var tcolumn in tcolumns)
+            {
+                var ttcol = new Dictionary<int, GameObject>();
+                ii++;
+                Debug.Log("" + jj + "," + ii + ": " + tcolumn);
+                // _WorldCells.Add
+
+                GameObject tt = Instantiate(_SpawnerPrefab);
+                float tx = 0 + (ii * (5f + 0.11f));
+                float ty = 0f;
+                float tz = 0 + (jj * (5f + 0.11f));
+                tt.transform.position = new Vector3(tx, ty, tz);
+                tt.GetComponent<Spawner>().SetMyObject(int.Parse(tcolumn));
+            }
+        }
+    }
+}
---
+++ Assets/xxx/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public List<GameObject> MyGameObjects;
+
+    public int _MyNumber = 0;
+
+    public void SetMyObject(int rindex) {
+        GameObject tt = Instantiate(MyGameObjects[rindex], this.transform );
+        // tt.transform.position = this.transform.position;
+        // tt.transform.parent = this.transform;
+        // MyGameObjects[rindex].SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
